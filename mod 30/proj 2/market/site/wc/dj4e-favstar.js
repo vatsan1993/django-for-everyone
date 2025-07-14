7d6921571fcdcff6df8a014256c7617fdad9fe33
Sozhavaram Srivatasan <sozhavaramsrivatsan@hotmail.com>
2025-07-14 10:49:27 +0530

    sample github settings added

---
+++ mod 30/proj 2/market/site/wc/dj4e-favstar.js
@@ -0,0 +1,24 @@
+import { html, LitElement } from "https://cdn.jsdelivr.net/npm/lit@3.2.1/+esm";
+
+export class DJ4EFavoriteStar extends LitElement {
+
+  static properties = {
+    fav: { type: Boolean },
+  };
+
+  // Don't use Shadow-DOM
+  createRenderRoot() { return this; }
+
+  render() {
+
+    return html`
+      <span class="fa-stack" style="vertical-align: middle;">
+        <i class="fa fa-star fa-stack-1x" style="${this.fav ? "" : "display: none;"} color: orange;"></i>
+        <i class="fa fa-star-o fa-stack-1x"></i>
+      </span>
+    `
+  }
+}
+
+customElements.define('dj4e-favstar', DJ4EFavoriteStar);
+
